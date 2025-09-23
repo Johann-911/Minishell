53356aa0f8bca956d6e6da9b39478a2d37359681
Klejdi Skenderaj <kskender@3-D-11.42heilbronn.de>
2025-09-23 22:19:31 +0200

    Finished pipex

---
+++ include/pipex/pipex.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kskender <kskender@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 14:10:24 by kskender          #+#    #+#             */
+/*   Updated: 2025/09/23 15:21:42 by kskender         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+void	handle_exit(int exit);
+
+#endif
