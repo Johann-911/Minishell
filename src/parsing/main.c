05a298db4e54b9f5c9dd041c524a7b39b1275df6
Jannis Toumanidis <jtoumani@3-F-9.42heilbronn.de>
2025-09-30 16:22:04 +0200

    lexing/parsing i think 90% syntax check done and a random main to test

--- src/parsing/main.c
+++ src/parsing/main.c
@@ -6,102 +6,51 @@
 /*   By: jtoumani <jtoumani@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 18:14:59 by jtoumani          #+#    #+#             */
-/*   Updated: 2025/09/25 18:55:52 by jtoumani         ###   ########.fr       */
+/*   Updated: 2025/09/26 14:25:02 by jtoumani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minishell.h"
 #include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	main(int argc, char **argv, char **env)
-
+static void	test_input(char *s)
 {
+	int	ok;
 
-
-
-	from_executor();
-
-
-	from_parser();
-
-
-	printf("This is a test from main\n");
-
-
-	printf("This is a test from main\n");
-
-
-	printf("This is a test from main\n");
-
-
-	char	*test_str;
-
-
-
-
-
-	(void)argc;
-
-
-	(void)argv;
-
-
-	(void)env;
-
-
-	gc_init();
-
-
-	while (1)
-
-
-	{
-
-
-		test_str = readline("Kayne West 🐐 > ");
-
-
-		if (test_str)
-
-
-		{
-
-
-			if (ft_strncmp(test_str, "exit", 4) == 0)
-
-
-			{
-
-
-				free(test_str);
-
-
-				break ;
-
-
-			}
-
-
-			printf("prompt : %s\n", test_str);
-
-
-			free(test_str);
-
-
-		}
-
-
-	}
-
-
-	clean_up();
-
-	return (0);
-
+	ok = check_tokens(s, 0);
+	printf("[%-3s] %s\n", ok ? "OK" : "ERR", s);
 }
 
+int	main(int argc, char **argv)
+{
+	char	*line;
+	size_t	len;
+	ssize_t	r;
 
-
-
-
-
+	line = NULL;
+	len = 0;
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+			test_input(argv[i]);
+		return (0);
+	}
+	printf("Syntax-Test (CTRL+D beendet)\n");
+	while (1)
+	{
+		printf("Zoro > ");
+		fflush(stdout);
+		r = getline(&line, &len, stdin);
+		if (r < 0)
+			break ;
+		if (r > 0 && line[r - 1] == '\n')
+			line[r - 1] = '\0';
+		if (*line == '\0')
+			continue ;
+		test_input(line);
+	}
+	free(line);
+	return (0);
+}
