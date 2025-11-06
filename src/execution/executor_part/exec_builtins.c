2e080e294cd520d548c1ee599e9fc460c23b05a2
Klejdi Skenderaj <kskender@3-H-7.42heilbronn.de>
2025-11-06 15:49:12 +0100

    Starting point at adding built ins

--- src/execution/executor_part/exec_builtins.c
+++ src/execution/executor_part/exec_builtins.c
@@ -6,139 +6,73 @@
 /*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 00:32:23 by klejdi            #+#    #+#             */
-/*   Updated: 2025/10/23 22:05:21 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/04 15:22:03 by klejdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
-#include "builtins.h"
-
-char **generate_env(t_env_list *env)
-{
-    char **envp;
-    t_env_node *cur;
-    int i = 0;
-
-    if (!env)
-        return (NULL);
-    envp = gc_malloc(sizeof(char *) * (env->size + 1));
-    if (!envp)
-        return (NULL);
-    cur = env->head;
-    while (cur)
-    {
-        char *tmp = ft_strjoin(cur->type, "=");
-        envp[i++] = ft_strjoin(tmp, cur->value);
-        gc_free(tmp);
-        cur = cur->next;
-    }
-    envp[i] = NULL;
-    return (envp);
-}
 
 int table_of_builtins(t_cmd_node *cmd, char **envp, int flag)
 {
-    if (!cmd || !cmd->cmd || !cmd->cmd[0])
-        return (-1);
-    if (!strcmp(cmd->cmd[0], "echo"))
-        return (ft_echo(cmd->cmd));
-    if (!strcmp(cmd->cmd[0], "pwd"))
-        return (ft_pwd(cmd->cmd));
-    if (!strcmp(cmd->cmd[0], "cd"))
-        return (ft_cd(cmd->cmd));
-    if (!strcmp(cmd->cmd[0], "export"))
-        return (ft_export(cmd->cmd));
-    if (!strcmp(cmd->cmd[0], "unset"))
-        return (ft_unset(cmd->cmd));
-    if (!strcmp(cmd->cmd[0], "env"))
-        return (ft_env(cmd->cmd));
-    /* not a builtin */
-    (void)envp;
-    (void)flag;
-    return (128);
-}
-
-/*
-** Checks if the argument is a valid -n option for echo
-*/
-static int is_n_option(char *arg)
-{
-    int i;
-
-    if (!arg || arg[0] != '-')
-        return (0);
-    i = 1;
-    while (arg[i])
-    {
-        if (arg[i] != 'n')
-            return (0);
-        i++;
-    }
-    return (i > 1);
-}
-
-/*
-** Prints the value of an environment variable if the argument starts with '$'
-*/
-static void echo_expand_and_print(char *arg)
-{
-    if (arg[0] == '$' && arg[1])
-    {
-        char *val = getenv(arg + 1);
-        if (val)
-            ft_putstr_fd(val, STDOUT_FILENO);
-    }
-    else
-        ft_putstr_fd(arg, STDOUT_FILENO);
-}
-
-/*
-** 42 norm-adaptable echo builtin with $VAR expansion
-*/
-static void echo_print_range(char **args, int start_index)
-{
-    int i;
-
-    i = start_index;
-    while (args[i])
-    {
-        echo_expand_and_print(args[i]);
-        if (args[i + 1])
-            ft_putchar_fd(' ', STDOUT_FILENO);
-        i++;
-    }
-}
-
-int ft_echo(char **args)
-{
-    int nl_option;
-    int start_index;
-
-    nl_option = 0;
-    start_index = 1;
-    if (args[1])
-    {
-        while (args[start_index] && is_n_option(args[start_index]))
-        {
-            nl_option = 1;
-            start_index++;
-        }
-    }
-    echo_print_range(args, start_index);
-    if (!nl_option)
-        ft_putchar_fd('\n', STDOUT_FILENO);
-    return (0);
+	if (!cmd || !cmd->cmd || !cmd->cmd[0])
+		return (-1);
+	if (!strcmp(cmd->cmd[0], "echo"))
+		return (ft_echo(cmd->cmd));
+	if (!strcmp(cmd->cmd[0], "pwd"))
+		return (ft_pwd(cmd->cmd));
+	if (!strcmp(cmd->cmd[0], "cd"))
+		return (ft_cd(cmd->cmd));
+	if (!strcmp(cmd->cmd[0], "export"))
+		return (ft_export(cmd->cmd));
+	if (!strcmp(cmd->cmd[0], "unset"))
+		return (ft_unset(cmd->cmd));
+	if (!strcmp(cmd->cmd[0], "env"))
+		return (ft_env(cmd->cmd));
+	/* not a builtin */
+	(void)envp;
+	(void)flag;
+	return (128);
 }
 
 int ft_pwd(char **args)
 {
-    char cwd[4096];
-    (void)args;
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        printf("%s\n", cwd);
-        return 0;
-    }
-    perror("pwd");
-    return 1;
+	char cwd[4096];
+
+	(void)args;
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		printf("%s\n", cwd);
+		return (0);
+	}
+	perror("pwd");
+	return (1);
+}
+
+/*
+** Quick compatibility shim for builtins: construct envp from the process
+** environ so that calls to setenv()/unsetenv() are immediately visible.
+*/
+char **generate_env(t_env_list *env)
+{
+	extern char **environ;
+	char **envp;
+	int i;
+
+	(void)env;
+	if (!environ)
+		return (NULL);
+	i = 0;
+	while (environ[i])
+		i++;
+	envp = gc_malloc(sizeof(char *) * (i + 1));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (environ[i])
+	{
+		envp[i] = ft_strdup(environ[i]);
+		i++;
+	}
+	envp[i] = NULL;
+	return (envp);
 }
--- src/execution/executor_part/exec_builtins1.c
+++ src/execution/executor_part/exec_builtins1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_builtins1.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kskender <kskender@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 00:07:02 by klejdi            #+#    #+#             */
-/*   Updated: 2025/10/24 16:03:43 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/03 14:17:01 by kskender         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,35 @@
 
 /* resolve_cd_target: set *target based on args and argc
    returns 0 on success, 1 on error (and prints an error message) */
-static int resolve_cd_target(char **args, int argc, char **target)
+static int	resolve_cd_target(char **args, int argc, char **target)
 {
-    if (argc > 2)
-    {
-        fprintf(stderr, "cd: too many arguments\n");
-        return (1);
-    }
-    if (argc == 1 || (args[1] && args[1][0] == '\0'))
-    {
-        *target = getenv("HOME");
-        if (!*target)
-        {
-            fprintf(stderr, "cd: HOME not set\n");
-            return (1);
-        }
-        return (0);
-    }
-    if (strcmp(args[1], "-") == 0)
-    {
-        *target = getenv("OLDPWD");
-        if (!*target)
-        {
-            fprintf(stderr, "cd: OLDPWD not set\n");
-            return (1);
-        }
-        return (0);
-    }
-    *target = args[1];
-    return (0);
+	if (argc > 2)
+	{
+		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
+		return (1);
+	}
+	if (argc == 1 || (args[1] && args[1][0] == '\0'))
+	{
+		*target = getenv("HOME");
+		if (!*target)
+		{
+			ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
+			return (1);
+		}
+		return (0);
+	}
+	if (strcmp(args[1], "-") == 0)
+	{
+		*target = getenv("OLDPWD");
+		if (!*target)
+		{
+			ft_putstr_fd("cd: OLDPWD not set\n", STDERR_FILENO);
+			return (1);
+		}
+		return (0);
+	}
+	*target = args[1];
+	return (0);
 }
 
 /*
@@ -55,28 +55,28 @@ static int resolve_cd_target(char **args, int argc, char **target)
  * - Permission denied: error
  * - Updates OLDPWD and PWD in environment
  */
-int ft_cd(char **args)
+int	ft_cd(char **args)
 {
-    char *target;
-    char oldpwd[PATH_MAX];
-    char newpwd[PATH_MAX];
-    int argc;
+	char	*target;
+	char	oldpwd[PATH_MAX];
+	char	newpwd[PATH_MAX];
+	int		argc;
 
-    argc = 0;
-    while (args[argc])
-        argc++;
-    if (!getcwd(oldpwd, sizeof(oldpwd)))
-        return (perror("cd: getcwd (oldpwd)"), 1);
-    /* determine target and validate args */
-    if (resolve_cd_target(args, argc, &target))
-        return (1);
-    if (chdir(target) != 0)
-        return (perror("cd"), 1);
-    if (!getcwd(newpwd, sizeof(newpwd)))
-        return (perror("cd: getcwd (newpwd)"), 1);
-    if (args[1] && strcmp(args[1], "-") == 0)
-        printf("%s\n", newpwd);
-    setenv("OLDPWD", oldpwd, 1);
-    setenv("PWD", newpwd, 1);
-    return (0);
+	argc = 0;
+	while (args[argc])
+		argc++;
+	if (!getcwd(oldpwd, sizeof(oldpwd)))
+		return (perror("cd: getcwd (oldpwd)"), 1);
+	/* determine target and validate args */
+	if (resolve_cd_target(args, argc, &target))
+		return (1);
+	if (chdir(target) != 0)
+		return (perror("cd"), 1);
+	if (!getcwd(newpwd, sizeof(newpwd)))
+		return (perror("cd: getcwd (newpwd)"), 1);
+	if (args[1] && strcmp(args[1], "-") == 0)
+		printf("%s\n", newpwd);
+	setenv("OLDPWD", oldpwd, 1);
+	setenv("PWD", newpwd, 1);
+	return (0);
 }
