cf39a782d6754d84100718dc14f8ceb7aa7b5707
Klejdi Skenderaj <kskender@3-H-7.42heilbronn.de>
2025-11-06 15:50:04 +0100

    Second point at adding built ins

--- src/execution/executor_part/exec_builtins2.c
+++ src/execution/executor_part/exec_builtins2.c
@@ -6,130 +6,139 @@
 /*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 17:31:20 by klejdi            #+#    #+#             */
-/*   Updated: 2025/10/17 17:15:32 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/05 21:18:20 by klejdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "executor.h"
-// Helper to strip quotes from value (for export)
+
 static char *strip_quotes(const char *value)
 {
-    int len = strlen(value);
-    if (len >= 2 && ((value[0] == '"' && value[len - 1] == '"') || (value[0] == '\'' && value[len - 1] == '\'')))
-    {
-        char *stripped = (char *)malloc(len - 1);
-        if (stripped)
-        {
-            strncpy(stripped, value + 1, len - 2);
-            stripped[len - 2] = '\0';
-        }
-        return stripped;
-    }
-    return NULL;
+	int len;
+	char *stripped;
+
+	len = ft_strlen(value);
+	if (len >= 2 && ((value[0] == '"' && value[len - 1] == '"') || (value[0] == '\'' && value[len - 1] == '\'')))
+	{
+		stripped = (char *)gc_malloc((size_t)len - 1);
+		if (stripped)
+		{
+			/* copy inner content, ensure null-termination */
+			ft_strlcpy(stripped, value + 1, (size_t)len - 1);
+			stripped[len - 2] = '\0';
+		}
+		return (stripped);
+	}
+	return (NULL);
 }
 
-/*
-** ft_export - Add or update environment variables
-** Handles:
-**   export VAR=value
-**   export VAR
-**   export (prints all env if no args)
-** Edge cases: invalid identifiers, multiple args, no '='
-*/
-
-/*
-** Checks if a string is a valid environment variable name
-*/
 int is_valid_identifier(const char *name)
 {
-    int i;
+	int i;
 
-    if (!name || !(name[0] == '_' || (name[0] >= 'A' && name[0] <= 'Z') || (name[0] >= 'a' && name[0] <= 'z')))
-        return (0);
-    i = 1;
-    while (name[i])
-    {
-        if (!(name[i] == '_' || (name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= 'a' && name[i] <= 'z') || (name[i] >= '0' && name[i] <= '9')))
-            return (0);
-        i++;
-    }
-    return (1);
+	if (!name || !(name[0] == '_' || (name[0] >= 'A' && name[0] <= 'Z') || (name[0] >= 'a' && name[0] <= 'z')))
+		return (0);
+	i = 1;
+	while (name[i])
+	{
+		if (!(name[i] == '_' || (name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= 'a' && name[i] <= 'z') || (name[i] >= '0' && name[i] <= '9')))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-// Checks if a variable is already in exported list
 static int is_in_exported(const char *name)
 {
-    int i = 0;
-    while (i < g_shell.exported_count)
-    {
-        if (strcmp(g_shell.exported_vars[i], name) == 0)
-            return 1;
-        i++;
-    }
-    return 0;
-}
+	int i;
 
-/*
-** ft_export - Add or update environment variables with full edge case handling
-*/
-// Add or update environment variables (<=25 lines)
-
-static void export_with_value(char *arg)
-{
-    char *eq;
-    char *value;
-    char *stripped;
-    eq = strchr(arg, '='); // first occurrence of '='
-    /*
-    ** Validate only the variable name (before '=') for export.
-    ** Temporarily replace '=' with '\0' to isolate the name, validate, then restore '='.
-    */
-    *eq = '\0';
-    if (!is_valid_identifier(arg))
-        printf("export: not a valid identifier: %s\n", arg);
-    else
-    {
-        value = eq + 1;
-        stripped = strip_quotes(value);
-        if (stripped)
-        {
-            setenv(arg, stripped, 1);
-            free(stripped);
-        }
-        else
-            setenv(arg, value, 1);
-    }
-    *eq = '='; // Restore '=' after validation and setting
+	i = 0;
+	while (i < g_shell.exported_count)
+	{
+		if (ft_strcmp(g_shell.exported_vars[i], name) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 static void export_no_value(char *arg)
 {
-    if (!is_valid_identifier(arg))
-        printf("export: not a valid identifier: %s\n", arg);
-    else if (!getenv(arg))
-    {
-        if (!is_in_exported(arg) && g_shell.exported_count < MAX_EXPORTED)
-            g_shell.exported_vars[g_shell.exported_count++] = strdup(arg);
-    }
+	if (!is_valid_identifier(arg))
+	{
+		ft_putstr_fd("export: not a valid identifier: ", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putchar_fd('\n', STDERR_FILENO);
+	}
+	else
+	{
+		if (!is_in_exported(arg) && g_shell.exported_count < MAX_EXPORTED)
+			g_shell.exported_vars[g_shell.exported_count++] = gc_strdup(arg);
+	}
+}
+
+static void export_with_value(char *arg)
+{
+	char *eq;
+	char *value;
+	char *stripped;
+	char *name;
+	size_t namelen;
+
+	eq = ft_strchr(arg, '=');
+	if (!eq)
+	{
+		/* no '=', behave like export no value */
+		export_no_value(arg);
+		return;
+	}
+	namelen = (size_t)(eq - arg);
+	name = (char *)gc_malloc(namelen + 1);
+	if (!name)
+		return;
+	memcpy(name, arg, namelen);
+	name[namelen] = '\0';
+
+	if (!is_valid_identifier(name))
+	{
+		ft_putstr_fd("export: not a valid identifier: ", STDERR_FILENO);
+		ft_putstr_fd(name, STDERR_FILENO);
+		ft_putchar_fd('\n', STDERR_FILENO);
+	}
+	else
+	{
+		value = eq + 1;
+		stripped = strip_quotes(value);
+		if (stripped)
+		{
+			setenv(name, stripped, 1);
+			gc_free(stripped);
+		}
+		else
+			setenv(name, value, 1);
+		/* track the name as exported */
+		if (!is_in_exported(name) && g_shell.exported_count < MAX_EXPORTED)
+			g_shell.exported_vars[g_shell.exported_count++] = gc_strdup(name);
+	}
 }
 
 int ft_export(char **args)
 {
-    int i;
-    i = 1;
-    if (!args[1])
-    {
-        print_exported_env();
-        return 0;
-    }
-    while (args[i])
-    {
-        if (strchr(args[i], '='))
-            export_with_value(args[i]);
-        else
-            export_no_value(args[i]);
-        i++;
-    }
-    return 0;
+	int i;
+
+	i = 1;
+	if (!args[1])
+	{
+		print_exported_env();
+		return (0);
+	}
+	while (args[i])
+	{
+		if (ft_strchr(args[i], '='))
+			export_with_value(args[i]);
+		else
+			export_no_value(args[i]);
+		i++;
+	}
+	return (0);
 }
--- src/execution/executor_part/exec_builtins3.c
+++ src/execution/executor_part/exec_builtins3.c
@@ -6,77 +6,86 @@
 /*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 18:06:11 by klejdi            #+#    #+#             */
-/*   Updated: 2025/10/15 19:44:04 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/04 16:49:37 by klejdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-// Add or update environment variables, bash-like
-
-// Print all exported variables (env + exported-but-unset)
 void print_exported_env(void)
 {
-    extern char **environ;
-    int i = 0;
-    char *eq;
-    while (environ[i])
-    {
-        eq = strchr(environ[i], '=');
-        if (eq)
-        {
-            *eq = '\0';
-            printf("declare -x %s=\"%s\"\n", environ[i], eq + 1);
-            *eq = '=';
-        }
-        else
-            printf("declare -x %s\n", environ[i]);
-        i++;
-    }
-    int j = 0;
-    while (j < g_shell.exported_count)
-    {
-        if (!getenv(g_shell.exported_vars[j]))
-            printf("declare -x %s\n", g_shell.exported_vars[j]);
-        j++;
-    }
+	extern char **environ;
+	int i;
+	char *eq;
+	int j;
+
+	i = 0;
+	while (environ[i])
+	{
+		eq = ft_strchr(environ[i], '=');
+		if (eq)
+		{
+			write(STDOUT_FILENO, "declare -x ", 11);
+			write(STDOUT_FILENO, environ[i], eq - environ[i]);
+			write(STDOUT_FILENO, "=\"", 2);
+			write(STDOUT_FILENO, eq + 1, ft_strlen(eq + 1));
+			write(STDOUT_FILENO, "\"\n", 2);
+		}
+		else
+		{
+			write(STDOUT_FILENO, "declare -x ", 11);
+			write(STDOUT_FILENO, environ[i], ft_strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		i++;
+	}
+	j = 0;
+	while (j < g_shell.exported_count)
+	{
+		if (g_shell.exported_vars[j] && !getenv(g_shell.exported_vars[j]))
+		{
+			write(STDOUT_FILENO, "declare -x ", 11);
+			write(STDOUT_FILENO, g_shell.exported_vars[j],
+				  ft_strlen(g_shell.exported_vars[j]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		j++;
+	}
 }
 
-// Print all environment variables (for env builtin)
 int ft_env(char **args)
 {
-    extern char **environ;
-    int i = 0;
-    (void)args;
-    while (environ[i])
-    {
-        printf("%s\n", environ[i]);
-        i++;
-    }
-    return 0;
+	extern char **environ;
+	int i;
+
+	i = 0;
+	(void)args;
+	while (environ[i])
+	{
+		ft_putstr_fd(environ[i], STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		i++;
+	}
+	return (0);
 }
 
-/*
-** ft_unset - Remove environment variables
-** Usage: unset VAR [VAR ...]
-** Edge cases: invalid identifiers, multiple args
-**this is for export just didnt have enough space in the other file
-*/
 int ft_unset(char **args)
 {
-    int i;
+	int i;
 
-    i = 1;
-    while (args[i])
-    {
-        if (!is_valid_identifier(args[i]))
-        {
-            printf("unset: not a valid identifier: %s\n", args[i]);
-            i++;
-            continue;
-        }
-        unsetenv(args[i]);
-        i++;
-    }
-    return (0);
+	i = 1;
+	while (args[i])
+	{
+		if (!is_valid_identifier(args[i]))
+		{
+			ft_putstr_fd("unset: not a valid identifier: ", STDERR_FILENO);
+			ft_putstr_fd(args[i], STDERR_FILENO);
+			ft_putchar_fd('\n', STDERR_FILENO);
+			i++;
+			continue;
+		}
+		unsetenv(args[i]);
+		i++;
+	}
+	return (0);
 }
