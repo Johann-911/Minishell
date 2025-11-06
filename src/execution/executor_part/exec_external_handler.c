ddc9f76a5ae9da132a400cd7f715846b18f77942
Klejdi Skenderaj <kskender@3-H-7.42heilbronn.de>
2025-11-06 15:51:10 +0100

    Adding external handler and also parent runner

--- src/execution/executor_part/exec_external_handler.c
+++ src/execution/executor_part/exec_external_handler.c
@@ -6,72 +6,11 @@
 /*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 17:34:28 by klejdi            #+#    #+#             */
-/*   Updated: 2025/10/24 16:03:39 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/06 03:21:48 by klejdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
-#include "builtins.h"
-/* forward declarations for static helpers defined below */
-static int create_pipes(int pipes[64][2], int ncmds);
-static int spawn_pipeline_children(char ***cmds, int ncmds, char **envp, int pipes[64][2], pid_t pids[64]);
-static void setup_child_io_and_exec(int idx, int ncmds, int pipes[64][2], int in_fd, int out_fd, char **cmd, char **envp);
-static void close_all_pipes(int pipes[64][2], int ncmds);
-static int wait_children(pid_t pids[64], int ncmds);
-// Searches $PATH for executable
-static char *find_in_path(char *cmd)
-{
-    char *path = getenv("PATH");
-    char *dirs[64];
-    static char fullpath[512];
-    int i = 0;
-
-    if (!path)
-        return (cmd);
-    char *p = strtok(path, ":");
-    while (p && i < 63)
-    {
-        dirs[i++] = p;
-        p = strtok(NULL, ":");
-    }
-    dirs[i] = NULL;
-    int j = 0;
-    while (dirs[j])
-    {
-        snprintf(fullpath, sizeof(fullpath), "%s/%s", dirs[j], cmd);
-        if (access(fullpath, X_OK) == 0)
-            return (fullpath);
-        j++;
-    }
-    return (cmd);
-}
-
-// Convert environment list to char ** array for execve
-char **convert_env_to_array(t_env_list *env_list)
-{
-    char **env_array;
-    t_env_node *current;
-    int i;
-    char *temp;
-
-    if (!env_list)
-        return (NULL);
-    env_array = gc_malloc(sizeof(char *) * (env_list->size + 1));
-    if (!env_array)
-        return (NULL);
-    current = env_list->head;
-    i = 0;
-    while (current)
-    {
-        temp = ft_strjoin(current->type, "=");
-        env_array[i] = ft_strjoin(temp, current->value);
-        gc_free(temp);
-        current = current->next;
-        i++;
-    }
-    env_array[i] = NULL;
-    return (env_array);
-}
 
 // Executes external command (execve in current process)
 void exec_external(char **args, char **envp)
@@ -82,121 +21,12 @@ void exec_external(char **args, char **envp)
     execve(exec_path, args, envp);
 }
 
-// Sets up and runs a simple pipeline (cmd1 | cmd2)
-int exec_pipeline(char ***cmds, int ncmds, char **envp)
-{
-    int pipes[64][2];
-    pid_t pids[64];
-
-    if (ncmds <= 0)
-        return (1);
-    if (create_pipes(pipes, ncmds) != 0)
-        return (1);
-    if (spawn_pipeline_children(cmds, ncmds, envp, pipes, pids) != 0)
-    {
-        close_all_pipes(pipes, ncmds);
-        return (1);
-    }
-    close_all_pipes(pipes, ncmds);
-    return wait_children(pids, ncmds);
-}
-
-static int create_pipes(int pipes[64][2], int ncmds)
-{
-    int i;
-
-    i = 0;
-    while (i < ncmds - 1)
-    {
-        if (pipe(pipes[i]) == -1)
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-static int spawn_pipeline_children(char ***cmds, int ncmds, char **envp, int pipes[64][2], pid_t pids[64])
-{
-    int i;
-
-    i = 0;
-    while (i < ncmds)
-    {
-        int in_fd = -1, out_fd = -1;
-        if (setup_redirections(cmds[i], &in_fd, &out_fd))
-            return (1);
-        pids[i] = fork();
-        if (pids[i] == 0)
-        {
-            setup_child_io_and_exec(i, ncmds, pipes, in_fd, out_fd, cmds[i], envp);
-        }
-        if (in_fd != -1)
-            close(in_fd);
-        if (out_fd != -1)
-            close(out_fd);
-        i++;
-    }
-    return (0);
-}
-
-static void setup_child_io_and_exec(int idx, int ncmds, int pipes[64][2], int in_fd, int out_fd, char **cmd, char **envp)
-{
-    int j;
-
-    if (idx > 0)
-        dup2(pipes[idx - 1][0], STDIN_FILENO);
-    if (in_fd != -1)
-        dup2(in_fd, STDIN_FILENO);
-    if (idx < ncmds - 1)
-        dup2(pipes[idx][1], STDOUT_FILENO);
-    if (out_fd != -1)
-        dup2(out_fd, STDOUT_FILENO);
-    j = 0;
-    while (j < ncmds - 1)
-    {
-        close(pipes[j][0]);
-        close(pipes[j][1]);
-        j++;
-    }
-    exec_external(cmd, envp);
-    _exit(127);
-}
-
-static void close_all_pipes(int pipes[64][2], int ncmds)
-{
-    int i;
-
-    i = 0;
-    while (i < ncmds - 1)
-    {
-        close(pipes[i][0]);
-        close(pipes[i][1]);
-        i++;
-    }
-}
-
-static int wait_children(pid_t pids[64], int ncmds)
-{
-    int i;
-    int status;
-
-    status = 0;
-    i = 0;
-    while (i < ncmds)
-    {
-        waitpid(pids[i], &status, 0);
-        i++;
-    }
-    if (WIFEXITED(status))
-        return WEXITSTATUS(status);
-    return 128;
-}
-
 // Sets up redirections for builtins and runs them
 int exec_builtin_with_redir(int (*builtin)(char **), char **args, int in_fd, int out_fd)
 {
     int saved_in = dup(STDIN_FILENO);
     int saved_out = dup(STDOUT_FILENO);
+    int ret;
     if (in_fd != -1)
     {
         dup2(in_fd, STDIN_FILENO);
@@ -207,7 +37,7 @@ int exec_builtin_with_redir(int (*builtin)(char **), char **args, int in_fd, int
         dup2(out_fd, STDOUT_FILENO);
         close(out_fd);
     }
-    int ret = builtin(args);
+    ret = builtin(args);
     dup2(saved_in, STDIN_FILENO);
     dup2(saved_out, STDOUT_FILENO);
     close(saved_in);
--- src/execution/executor_part/exec_parent_runner.c
+++ src/execution/executor_part/exec_parent_runner.c
@@ -6,12 +6,18 @@
 /*   By: klejdi <klejdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 13:14:57 by kskender          #+#    #+#             */
-/*   Updated: 2025/10/23 22:05:19 by klejdi           ###   ########.fr       */
+/*   Updated: 2025/11/04 15:39:10 by klejdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
-#include "builtins.h"
+
+/*
+1.sets input/output redirections for a comman and checks for errors
+2. handles both input and output redirections with error checking
+3. Returns EXIT_SUCCESS on success, or error code on failure
+4. uses GC-tracked file descriptors to prevent leaks
+*/
 
 static int setting_infile_and_outfile(t_cmd_node *cmd, int *io_data)
 {
@@ -20,7 +26,7 @@ static int setting_infile_and_outfile(t_cmd_node *cmd, int *io_data)
 	error_code = 0;
 	if (io_data[0] != NO_REDIRECTION)
 	{
-		error_code = setup_input_file((t_commandlist *)cmd);
+		error_code = setup_input_file_from_cmd(cmd);
 		io_data[2] = NO_REDIRECTION;
 		io_data[4] = NO_REDIRECTION;
 		if (error_code != EXIT_SUCCESS)
@@ -28,7 +34,7 @@ static int setting_infile_and_outfile(t_cmd_node *cmd, int *io_data)
 	}
 	if (io_data[1] != NO_REDIRECTION)
 	{
-		error_code = setup_output_file((t_commandlist *)cmd);
+		error_code = setup_output_file_from_cmd(cmd);
 		io_data[2] = NO_REDIRECTION;
 		io_data[4] = NO_REDIRECTION;
 		if (error_code != EXIT_SUCCESS)
@@ -37,63 +43,14 @@ static int setting_infile_and_outfile(t_cmd_node *cmd, int *io_data)
 	return (error_code);
 }
 
-static int is_builtin_cmd(char **argv)
-{
-	if (!argv || !argv[0])
-		return 0;
-	if (!strcmp(argv[0], "echo"))
-		return 1;
-	if (!strcmp(argv[0], "cd"))
-		return 1;
-	if (!strcmp(argv[0], "pwd"))
-		return 1;
-	if (!strcmp(argv[0], "export"))
-		return 1;
-	if (!strcmp(argv[0], "unset"))
-		return 1;
-	if (!strcmp(argv[0], "env"))
-		return 1;
-	return 0;
-}
-
 pid_t exec_parent_runner(t_cmd_node *cmd, int *io_data)
 {
 	int error_code;
-	char **envp;
-	pid_t pid;
 
 	error_code = setting_infile_and_outfile(cmd, io_data);
 	dup_and_or_close(&io_data[2], &io_data[4]);
 	if (error_code != EXIT_SUCCESS)
-		return (-1);
-
-	/* If this is a builtin, run it in the parent and return 0 (no child pid) */
-	if (is_builtin_cmd(cmd->cmd))
-	{
-		envp = generate_env(cmd->env);
-		table_of_builtins(cmd, envp, 1);
-		return 0;
-	}
-
-	pid = fork();
-	if (pid == 0)
-	{
-		if (io_data[0] != NO_REDIRECTION)
-		{
-			dup2(io_data[0], STDIN_FILENO);
-			close(io_data[0]);
-		}
-		if (io_data[1] != NO_REDIRECTION)
-		{
-			dup2(io_data[1], STDOUT_FILENO);
-			close(io_data[1]);
-		}
-		envp = generate_env(cmd->env);
-		exec_external(cmd->cmd, envp);
-		_exit(127);
-	}
-	if (pid > 0)
-		return pid;
-	perror("fork");
-	return -1;
+		return (*(cmd->env->pid));
+	error_code = table_of_builtins(cmd, generate_env(cmd->env), 1);
+	return (*(cmd->env->pid));
 }
